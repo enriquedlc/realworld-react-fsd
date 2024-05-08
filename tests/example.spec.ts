593408710a815e2ec70edbf2c01b0a5f8bbe6168
Enrique De La Concepción <enriquedlc2d@gmail.com>
2024-05-08 17:44:22 +0000

    added ci

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // go to localhost:8123
+  await page.goto('http://localhost:8123');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  // Seleccionar el segundo botón "Continue" por su posición
+  const button = await page
+    .getByRole('button', {
+      name: 'Continue',
+    })
+    .click();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  console.log(button);
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page).toHaveTitle('conduit');
 });
