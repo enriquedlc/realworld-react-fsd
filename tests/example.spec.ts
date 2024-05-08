c329a5573cd4c14ae5b6072acd933eb9cb013474
juan pablo claros <juan_pablo_claros@hotmail.com>
2024-05-08 19:22:39 +0100

    test passed

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,16 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   // go to localhost:8123
-  await page.goto('http://localhost:8123');
+  await page.goto('http://localhost:5173');
 
-  // Seleccionar el segundo botón "Continue" por su posición
-  const button = await page
-    .getByRole('button', {
-      name: 'Continue',
-    })
-    .click();
-
-  console.log(button);
-
-  await expect(page).toHaveTitle('conduit');
+  await expect(page).toHaveTitle(/conduit/i);
 });
