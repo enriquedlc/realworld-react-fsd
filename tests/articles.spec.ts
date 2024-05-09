93dda816accd5e0d13aaf26d2f2b921ddb9cdf61
enriquedlc <enriquedlc.dev@gmail.com>
2024-05-09 18:52:44 +0100

    added todo

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -3,11 +3,20 @@ import { Article } from './pom/Article';
 import { NewArticle } from './pom/NewArticle';
 
 test('write article', async ({ page }) => {
+  // todo: before deleting an article
+  // go to / and view the global feed
+  // then click on the article to view it
+  // then delete it
   const article = new Article(page);
   const newArticle = new NewArticle(page);
 
   await newArticle.navigate();
   await newArticle.create();
 
+  await page
+    .getByRole('link', { name: 'The Impact of Renewable' })
+    .first()
+    .click();
+  await page.getByRole('button', { name: 'Delete Article' }).first().click();
   await article.expectToBeVisible();
 });
