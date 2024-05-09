9e0ba0d325726b0bc959bae55a1baea6dd33c237
enriquedlc <enriquedlc.dev@gmail.com>
2024-05-09 17:55:47 +0100

    refactor: auth test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,24 +1,7 @@
 import { test } from '@playwright/test';
-import { SignUp } from './SignUp';
-import { SignIn } from './SignIn';
-import { ALICE } from './fixtures/users';
+
+import { loginOrSignUp } from './pom/utils';
 
 test('Sign up Alice', async ({ page }) => {
-  const signUp = new SignUp(page);
-
-  await page.goto('/');
-  try {
-    await page
-      .getByRole('button', { name: 'Your Feed' })
-      .waitFor({ timeout: 300 });
-  } catch (error) {
-    const signIn = new SignIn(page);
-    await signIn.navigate();
-    try {
-      await signIn.signIn();
-    } catch (error) {
-      await signUp.navigate();
-      await signUp.signUp();
-    }
-  }
+  await loginOrSignUp(page);
 });
---
+++ tests/pom/utils.ts
@@ -0,0 +1,23 @@
+import { Page } from '@playwright/test';
+import { SignUp } from '../SignUp';
+import { SignIn } from '../SignIn';
+
+export async function loginOrSignUp(page: Page) {
+  const signUp = new SignUp(page);
+  const signIn = new SignIn(page);
+
+  await page.goto('/');
+  try {
+    await page
+      .getByRole('button', { name: 'Your Feed' })
+      .waitFor({ timeout: 300 });
+  } catch (error) {
+    await signIn.navigate();
+    try {
+      await signIn.signIn();
+    } catch (error) {
+      await signUp.navigate();
+      await signUp.signUp();
+    }
+  }
+}
